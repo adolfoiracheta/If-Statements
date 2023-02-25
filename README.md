645c552a37ad7b3cf6894d2622416d75bfebbb44
adolfoiracheta <84049381+adolfoiracheta@users.noreply.github.com>
2023-02-24 17:04:39 -0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# If-Statements
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-eif1y9)
---
+++ index.js
@@ -0,0 +1,30 @@
+/*
+//create three variables
+//create two arrays
+//we have a box (input), inside have a string called 'Rap'
+//if input equals an item in an  first array, print an item from second group
+
+let myMusic = ['Rap','R&B','Funk','Blues'];
+let myGroups = ['RunDMC','Afrika Bambatta','WuTangClan','BDP'];
+
+//this is what will change
+let input = 'Rap'
+
+//this is the condition
+if (input == myMusic[0]) {
+  //various methods to print items
+  //console.log(myMusic)
+  //console.log('My favorite group is ' + myGroups[0])
+  console.log('My favorite group is' +' ' + myGroups[0])
+}
+*/
+
+let mySubjects = ['Post Production','Software Development','CyberSecurity'];
+let myGrades = ['A','B','C','D','F'];
+let input = 'Software Development'
+
+if (input == mySubjects[1]) {
+  console.log('My grade is ' + myGrades[0]);
+}
+
+
